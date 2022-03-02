ccf15cc0b2137d884450ae0598c879fb9faf2329
evedelek <vedelekedina@gmail.com>
2022-03-02 16:47:14 +0100

    while loop

---
+++ while_loop.py
@@ -0,0 +1,35 @@
+# logikai = boolean
+# két értéke lehet: true, false  - literál
+
+are_you_a_boy = True
+print(are_you_a_boy)
+
+
+result = 10 < 20
+print(result)
+
+result = 10 < 20
+print(result)
+
+result = 10 ==20
+print(result)
+
+#végtelen ciklus
+#while True:
+    #print("Hello ciklus")
+
+
+#count = 0
+#while count < 10:
+    #print("heLlo" + str(count))
+    #count = count + 1
+
+#addig kérj be a felhasználótól egy számot, amíg 0-át nem ír be
+# írd ki a bekért szám kétszeresét
+
+
+number = 10
+while number != 0:
+    number = int(input("Adj meg egy számot:"))
+    print(number * 2)
+
